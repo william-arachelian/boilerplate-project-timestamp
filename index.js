388a0b526bb9117566950976ae639d535114e032
Will Arachelian <william.arachelian@gmail.com>
2024-05-13 11:12:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,85 +26,38 @@ app.get("/api/hello", function (req, res) {
 app.get("/api", function(req, res) {
 
     let currDate = new Date();
-    let timestamp = currDate.valueOf();
+    let timestamp = currDate.getTime();
     let dateString = currDate.toUTCString();
 
     return res.json({unix: timestamp, utc: dateString});
 });
 
-app.get("/api/:date", function(req, res) {
+app.get("/api/:date?", function(req, res) {
   try {
+    let date_string = req.params.date;
+    let dateObj;
     let timestamp;
     let dateString;
 
-    if (isValidUnix(req.params.date)) {
-      timestamp = parseInt(req.params.date);
-      dateString = new Date(timestamp).toUTCString();
+    if (!date_string.includes("-")) {
+      dateObj = new Date(parseInt(date_string));
     }
-    else if (isValidDate(req.params.date)) {
-      let date = new Date(req.params.date);
-      timestamp = date.valueOf();
-      dateString = date.toUTCString();
+    
+    else dateObj = new Date(date_string);
+    if (isNaN(dateObj.getTime())) throw "Invalid Date";
 
-    }
-
-    else throw "Invalid Date";
+    timestamp = dateObj.getTime();
+    dateString = dateObj.toUTCString();
 
     return res.json({unix: timestamp, utc: dateString});
   }
   catch(e) {
-    return res.json({ error : "Invalid Date" });
+    return res.json({ error : e });
   }
  
-
 });
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-let isValidDate = (input) => {
-  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  if (typeof input != 'string') return false;
-  if (input.split("-").length != 3) return false;
-
-  //preprocessing input
-  let dateSplit = input.split("-");
-  if (dateSplit[1].length == 1) dateSplit[1] = "0" + dateSplit[1];
-  if (dateSplit[2].length == 1) dateSplit[2] = "0" + dateSplit[1];
-
-  let year = parseInt(dateSplit[0]);
-  let month = parseInt(dateSplit[1]);
-  let day = parseInt(dateSplit[2]);
-
-  let currentYear = new Date().getFullYear();
-
-  //year validation
-  if (dateSplit[0].length != 4 || year == NaN) return false;
-  if (year > currentYear) return false;
-
-  //month validation
-  if (dateSplit[2].length != 2 || month == NaN) return false;
-  if (month < 1 || month > 12) return false;
-
-  //day validation
-  if (dateSplit[2].length != 2 || day == NaN) return false;
-  //leap year
-  if (month == 2 && year % 4 == 0) {
-    if (day < 1 || day > 29) return false;
-  }
-  else if (day < 1 || day > daysInMonth[month - 1]) return false;
-
-  return true;
-};
-
-let isValidUnix = (input) => {
-  if (typeof input != 'string') return false;
-  if (parseInt(input) == NaN) return false
-  if (input.length != 13) return false
-  return true;
-
-};
